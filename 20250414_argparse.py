51cbc650a68b95545e4bf263615cbeb5ac62894f
Keshan Pillay <kpillay@hotmail.co.nz>
2025-04-29 09:11:52 +1200

    Completes required functionality

--- 20250414_argparse.py
+++ 20250414_argparse.py
@@ -1,6 +1,6 @@
 import argparse
 
-parser = argparse.ArgumentParser
+parser = argparse.ArgumentParser()
 
 parser.add_argument("echo")
 args = parser.parse_args()
--- 20250427/20250427_loganalysis.py
+++ 20250427/20250427_loganalysis.py
@@ -18,44 +18,41 @@ now = now.replace(microsecond=0)
 
 recent_now = now - timedelta(minutes=5)
 
+MIN_ERROR = 10
+
+# DEBUG
 print(f"The time now is: {now}")
-print(f"The time 5 mins ago was: {recent_now} Big fat hen")
+print(f"The time 5 mins ago was: {recent_now}")
 
 print(args.filename)
 
 def scan_logs(filename):
-    print("hello")
-
     with open(filename) as file:
 
         count = 0
         recent_count = 0 # To track errors within the last 5 mins
 
-     
-
-
-
         for line in file:
-            print(line)
             procline = line.split("]")
             date = procline[0][1:]
-            #print(date)
             line_datetime = datetime.strptime(date, '%Y-%m-%d %H:%M:%S')
-            if "ERROR" in line:
+            if "ERROR".lower() in line.lower():
                 count += 1
-                
 
             if line_datetime > recent_now and line_datetime < now: # The 'and' wouldn't be required as in real situation we wouldn't have log entries 'in the future'
-                print("within 5 mins")
-                recent_count += 1
+                #print("within 5 mins")
+                if "ERROR".lower() in line.lower():
+                    recent_count += 1
 
+            # DEBUG
             # print("hmm:" ,line_datetime)
             # print("now:" , recent_now)
         
         print(f"The error count is: {count}")
-
-    f = open(f"error_{filename}", "x")
-    f.write(f"There have been {recent_count} errors in {filename} in the last 5 mins")
+    
+    if recent_count > MIN_ERROR:
+        f = open(f"error_{filename}", "x")
+        f.write(f"There have been {recent_count} errors in {filename} in the last 5 mins")
     
 
 
