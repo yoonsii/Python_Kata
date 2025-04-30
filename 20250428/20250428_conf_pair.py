86a6f06c317a76978653aea8891bc9c20f168022
yoonsii <kpillay@hotmail.co.nz>
2025-04-30 15:50:12 +1200

    fixes names and protects against empty lines

--- 20250428/20250428_conf_pair.py
+++ 20250428/20250428_conf_pair.py
@@ -4,21 +4,25 @@ import argparse
 VALID_KEYS = ['username', 'password', 'host', 'port']
 
 def process_line(line, keys_dict):
-    splitline = line.replace("\n","").split("=")
-    keys_dict[splitline[0]] = splitline[1]
+    if not (line == "" or "\n"):
+        splitline = line.replace("\n","").split("=")
+        keys_dict[splitline[0]] = splitline[1]
     return keys_dict
     
 def find_missing_keys(keys_dict):
     missing = [word for word in VALID_KEYS if word not in keys_dict]
     return missing
 
-def find_missing_values(keys_dict):
+def find_existing_keys(keys_dict):
     present = [word for word in VALID_KEYS if word in keys_dict]
     return present
 
+# Optional Not in Spec but made for learning purposes
 def print_missing_values(keys_dict):
+    # print('test')
     for key in keys_dict:
-        print(key, end=" ")
+        if keys_dict[key] == "":
+            print(f"The key: {key} is missing a value.")
 
 
 
@@ -26,20 +30,25 @@ def main():
 
     conf_pairs = {}
 
-    parser = argparse.ArgumentParser()
-    parser.add_argument("filename")
-    args = parser.parse_args()
-    filename = args.filename
+    # parser = argparse.ArgumentParser()
+    # parser.add_argument("filename")
+    # args = parser.parse_args()
+    # filename = args.filename
+    filename = "db1.conf"
 
-    print(f"Filename: {filename}")
+    print(f"File: {filename}")
+
 
     with open(filename) as file:
         for line in file:
             process_line(line, conf_pairs)
 
-    missing = find_missing_keys(conf_pairs)
-    missing_values = find_missing_values(conf_pairs)
-    print(f"Missing: {', '.join(missing_values)}")
+
+    missing_keys = find_missing_keys(conf_pairs)
+
+    # print_missing_values(conf_pairs)
+
+    print(f"Missing: {', '.join(missing_keys)}")
 
 if __name__ == "__main__":
     main()
