1f430fb6f32156caff8cc754b88197f040357e17
yoonsii <kpillay@hotmail.co.nz>
2025-05-21 08:07:21 +0000

    starting subprocess exercise refinements

---
+++ 20250511_SubProcess_Check_Tool/20250519_SubProcess_refined.py
@@ -0,0 +1,37 @@
+import subprocess
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("filename")
+args = parser.parse_args()
+
+filename = args.filename  
+
+def process_lines(line):
+    
+    output = subprocess.run(line, capture_output=True, shell=True, text=True)
+    firstline = ""
+    print(f"Command: {line.strip()}")
+    print(f"Return Code: {output.returncode}")
+    if (output.stdout != ""):
+        firstline, *_ = output.stdout.split("\n",1)
+    else:
+        firstline, *_ = output.stderr.split("\n",1)
+
+    print(f"Output: {firstline}\n")
+
+
+def main():
+    with open(filename) as file:
+       for line in file:
+            # Improvement: Skip any line that is blank (rather than forking empty)
+            cmd = line.strip() 
+            if not cmd or cmd.startswith('#'):
+                continue
+            process_lines(cmd)
+
+
+
+if __name__ == "__main__":
+    main()
+
