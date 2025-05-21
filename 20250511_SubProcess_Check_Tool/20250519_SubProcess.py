862f828279b0043cd693bfa45d2f1558f009adda
yoonsii <kpillay@hotmail.co.nz>
2025-05-21 06:51:00 +0000

    Subprocess exercise works functionally but needs 'devops' refinements

--- 20250511_SubProcess_Check_Tool/20250519_SubProcess.py
+++ 20250511_SubProcess_Check_Tool/20250519_SubProcess.py
@@ -5,42 +5,28 @@ parser = argparse.ArgumentParser()
 parser.add_argument("filename")
 args = parser.parse_args()
 
-def main():
-    print(args.filename)
-    filename = args.filename
+filename = args.filename  
 
-    #with open(filename) as file:
-    #    for line in file:
-    #        print(line)
-
-    with open(filename) as file:
-        line = file.readline()
-
-        command_components = line.split()
-
-
-    # print(line)
-    print(command_components)
-
-    # Note - 
-
+def process_lines(line):
+    command_components = line.split()
     output = subprocess.run(line, capture_output=True, shell=True, text=True)
-    print(output)
-
     firstline = ""
-
-
-    print(firstline)
-
-
-    print(f"Command: {line}")
+    print(f"Command: {line.strip()}")
     print(f"Return Code: {output.returncode}")
     if (output.stdout != ""):
         firstline, *_ = output.stdout.split("\n",1)
     else:
         firstline, *_ = output.stderr.split("\n",1)
 
-    print(f"Output: {firstline}")
+    print(f"Output: {firstline}\n")
+
+
+def main():
+    with open(filename) as file:
+       for line in file:
+        #    print(line)
+           process_lines(line)
+
 
 
 if __name__ == "__main__":
