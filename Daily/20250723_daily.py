1c49d2907b83bfd733381224cce726b7e637d92c
yoonsii <kpillay@hotmail.co.nz>
2025-07-23 05:11:09 +0000

    [Progress]: Nearly complete on batch 2. Need to work out the conditional function

--- Daily/20250723_daily.py
+++ Daily/20250723_daily.py
@@ -12,3 +12,51 @@ def count_uppercase(s):
 print(count_uppercase("Hey Guys hOw yo GOING"))
 
 
+# Batch 2: Lists, Logic, and Reusability
+# Write a function all_even(lst) that returns True if all elements in the list are even.
+# Write a function filter_long_words(words, length) that returns only the words longer than length. Use a list comprehension.
+# Write a function running_total(lst) that returns a list where each element is the sum of all previous elements plus itself.
+#  Example: [1,2,3] → [1,3,6]
+# Write a function find_max_index(lst) that returns the index of the largest element.
+# Write a function apply_if(func, lst, condition) that applies func to each element only if it meets the condition. Use list comprehension.
+
+def all_even(lst):
+    return sum(lst) % 2 == 0
+
+lst = [2,2,4,6,8,1]
+
+print(all_even(lst))
+
+def filter_long_words(words,length):
+    return [word for word in words if len(word) > length]
+
+w = ["a", "b", "cc", "dearie", "elephants"]
+
+print(filter_long_words(w, 2))
+
+lst = [1,2,3,4]
+
+def running_total(lst):
+    output = []
+    for i, num in enumerate(lst):
+        output.append(sum(lst[:num]))
+    return output
+
+print(running_total(lst))
+
+def find_max_index(lst):
+    return max(lst)
+
+print(find_max_index(lst))
+
+# However in the spirit of study I will do it manually as well
+# Choosing not to sort as that may be computationally bad, whereas a single pass is only O(n)
+
+def find_max_index_alt(lst):
+    max = 0
+    for i in lst:
+        if i > max:
+            max = i
+    return max
+
+
