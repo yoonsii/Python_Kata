8a3dacf9ff032e47704dd658396a3a7ac22a6bd2
yoonsii <kpillay@hotmail.co.nz>
2025-08-05 03:52:28 +0000

    [Progress]: Batch 1 Complete

--- Daily/20250801_daily.py
+++ Daily/20250801_daily.py
@@ -4,3 +4,35 @@ x = lambda a : str(a)
 lst = [1,2,3,4,7]
 
 print(list(map(lambda a:str(a), lst)))
+
+
+# From a list of words, return only those with more than 4 characters.
+
+x = lambda a : len(a) > 4
+words = ["tree", "sunshine", "cat", "mountain", "sky", "apple"]
+
+print(list(filter(x, words))) # could have put the function in here without a variable but am experimenting
+
+# Exercise 3: Enumerate & Index Search
+# Return the index of the first word in a list that starts with the letter "a".
+words = ["sun", "cat", "apple", "banana", "avocado"]
+
+def exercise_three(lst):
+    for i, word in enumerate(words):
+        if word.lower()[0] == "a":
+            return i
+    return
+
+print(exercise_three(words))
+
+
+names = ["Alice", "Bob", "Charlie"]
+scores = [85, 92, 78]
+
+combined_names_scores = zip(names,scores)
+
+# for x in combined_names_scores:
+#     print(x[0])
+
+output = [f"{x[0]}:{x[1]}" for x in combined_names_scores]
+print(output)
