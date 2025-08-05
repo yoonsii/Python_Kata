e9458eae83691b85fa9468b5ee661432f2d56d31
yoonsii <kpillay@hotmail.co.nz>
2025-08-05 05:28:19 +0000

    [Progress]: Batch 2 Nearly Complete

--- Daily/20250805_daily.py
+++ Daily/20250805_daily.py
@@ -1,28 +1,37 @@
+from utils_20250805 import sum_all
 # 6. Basic Arg Unpacking
 # Function that returns the sum of any number of args using *argso
 
-def sum_all(*args):
-    count = 0
-    for i in args:
-        count += i
-    return count
+# def sum_all(*args): #commented for #8
+#     count = 0
+#     for i in args:
+#         count += i
+#     return count
 
-print(sum_all(1,2,3,7))
+# print(sum_all(1,2,3,7))
 
 # 7. Kwarg Info Formatter
 # Function that accepts **kwargs and prints nicely formatted key:value pairs
 
 def print_info(**kwargs):
     for key, value in kwargs.items():
-        print(f"{key} ---> {value}")
+        print(f"{key} --> {value}")
 
 print_info(name="Alice", age=30, job="Engineer")
 
 # 8. Refactor into Module
 # Move a utility function into a separate .py file and import it
 
+print(sum_all(2,4,6,8))
+
 # 9. Tuple Unpacking Puzzle
 # Extract all names from [("John", 25), ("Jane", 30), ...]
+
+people = [("John", 25), ("Jane", 30), ("Alice", 28), ("Bob", 40)]
+
+lst = [name for name, age in people]
+
+print(lst)
 
 # 10. Dict Merge Tool
 # Merge two kwargs-style dicts using ** unpacking
---
+++ Daily/utils_20250805.py
@@ -0,0 +1,5 @@
+def sum_all(*args):
+    count = 0
+    for i in args:
+        count += i
+    return count
